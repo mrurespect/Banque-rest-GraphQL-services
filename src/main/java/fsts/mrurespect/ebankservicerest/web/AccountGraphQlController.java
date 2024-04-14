6f52032bcfad5265f16ee8852f099c43ed97756f
EL FADILI MOHAMED <580.simo@gmail.com>
2024-04-14 00:55:12 +0000

    adding graphQL exception resolver

--- src/main/java/fsts/mrurespect/ebankservicerest/web/AccountGraphQlController.java
+++ src/main/java/fsts/mrurespect/ebankservicerest/web/AccountGraphQlController.java
@@ -1,7 +1,11 @@
 package fsts.mrurespect.ebankservicerest.web;
 
+import fsts.mrurespect.ebankservicerest.dto.AccountRequestDto;
+import fsts.mrurespect.ebankservicerest.dto.AccountResponseDto;
 import fsts.mrurespect.ebankservicerest.entity.Account;
 import fsts.mrurespect.ebankservicerest.service.AccountService;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -18,5 +22,14 @@ public class AccountGraphQlController {
     @QueryMapping
     public List<Account> accountList() {
         return accountService.getAllAccounts();
+    }
+    @QueryMapping
+    public Account account(@Argument  String  id){
+        return accountService.getAccountById(id);
+    }
+
+    @MutationMapping
+    public AccountResponseDto addAccount(@Argument AccountRequestDto account){
+        return accountService.addAccount(account);
     }
 }
